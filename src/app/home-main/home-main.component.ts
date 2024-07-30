f807c102a352e3f0bfa9359b4b07f963191f1541
Juan Lagunas <dev.jlagunas@gmail.com>
2024-07-30 13:11:59 +0900

    feat(HomeMain): set up html structure

--- src/app/home-main/home-main.component.ts
+++ src/app/home-main/home-main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFaqComponent } from './home-faq/home-faq.component';
 
 @Component({
   selector: 'app-home-main',
   standalone: true,
-  imports: [],
+  imports: [HomeHeaderComponent, HomeFaqComponent],
   templateUrl: './home-main.component.html',
-  styleUrl: './home-main.component.scss'
+  styleUrl: './home-main.component.scss',
 })
-export class HomeMainComponent {
-
-}
+export class HomeMainComponent {}
