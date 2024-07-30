d53128028963718fbc7f6211ae0e1024ac83b94d
Juan Lagunas <dev.jlagunas@gmail.com>
2024-07-30 13:03:25 +0900

    create home-main, home-header, home-faq components

---
+++ src/app/home-main/home-faq/home-faq.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-faq',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-faq.component.html',
+  styleUrl: './home-faq.component.scss'
+})
+export class HomeFaqComponent {
+
+}
