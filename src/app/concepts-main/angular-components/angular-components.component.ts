8f87bb7760040a7d8fbac2b9532cdb3d1b2363d1
Juan Lagunas <dev.jlagunas@gmail.com>
2024-08-01 19:39:00 +0900

    feat(AngularComponents): add component html structure and basic styling with dynamic populated data

--- src/app/concepts-main/angular-components/angular-components.component.ts
+++ src/app/concepts-main/angular-components/angular-components.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { Subcategory } from '../../interfaces/subcategory';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-angular-components',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './angular-components.component.html',
   styleUrl: './angular-components.component.scss',
 })
-export class AngularComponentsComponent implements OnInit {
-  categoryId!: string | null;
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.categoryId = params.get('id');
-    });
-  }
+export class AngularComponentsComponent {
+  subcategories: Subcategory[] = [
+    {
+      id: '1-1',
+      name: 'Component Essentials',
+      description: 'Learn the basics of components.',
+      link: '/components/essentials',
+      borderColor: 'border-l-red-500',
+    },
+    {
+      id: '1-2',
+      name: 'Component Communication',
+      description: 'Learn how components communicate.',
+      link: '/components/communication',
+      borderColor: 'border-l-green-500',
+    },
+    {
+      id: '1-3',
+      name: 'Content Projection',
+      description: 'Learn about content projection.',
+      link: '/components/projection',
+      borderColor: 'border-l-blue-500',
+    },
+    {
+      id: '1-4',
+      name: 'Lifecycle Hooks',
+      description: 'Understand lifecycle hooks.',
+      link: '/components/lifecycle',
+      borderColor: 'border-l-yellow-500',
+    },
+  ];
 }
---
+++ src/app/interfaces/subcategory.ts
@@ -0,0 +1,7 @@
+export interface Subcategory {
+  id: string;
+  name: string;
+  description: string;
+  link: string;
+  borderColor: string;
+}
