d6e000f4208b19229d541b29c7260795b1c1654a
Juan Lagunas <dev.jlagunas@gmail.com>
2024-08-02 13:00:33 +0900

    feat(ComponentUsage): add html structure and content then style template.

--- src/app/concepts-main/angular-components/component-communication/component-usage/component-usage.component.ts
+++ src/app/concepts-main/angular-components/component-communication/component-usage/component-usage.component.ts
@@ -11,10 +11,41 @@ import { CodeBlockComponent } from '../../../../shared/code-block/code-block.com
 export class ComponentUsageComponent {
   codeSelector: string = `
    @Component({
-      selector: 'my-component',
-      templateUrl: 'my-component.html',
-      styleUrls: ['my-component.css'],
       standalone: true,
+      selector: 'my-component-a',
+      templateUrl: 'my-component-a.html',
+      styleUrls: ['my-component-a.css'],
+    })
+    export class MyComponent { }
+  `;
+
+  codeParent: string = `
+    @Component({
+      standalone: true,
+      imports: [MyComponentA],
+      selector: 'my-component-b',
+      templateUrl: '
+        <p>Check out my child component below!</p>
+        <my-component-a></my-component-a>
+      ',
+      styleUrls: ['my-component-b.css'],
+    })
+    export class MyComponent { }
+  `;
+
+  codeImport: string = `
+    import { Component } from '@angular/core';
+    import { MyComponentA } from './path-to-my-component-a/my-component-a.component';
+
+    @Component({
+      standalone: true,
+      imports: [MyComponentA],
+      selector: 'my-component-b',
+      templateUrl: '
+        <p>Check out my child component below!</p>
+        <my-component-a></my-component-a>
+      ',
+      styleUrls: ['my-component-b.css'],
     })
     export class MyComponent { }
   `;
