236cd3679f4e80a16b15a8845e2ab38118b7e0eb
Juan Lagunas <dev.jlagunas@gmail.com>
2024-08-05 10:18:57 +0900

    feat(OutputCommunication): complete html content and styling for this component

--- src/app/concepts-main/angular-components/component-communication/output-communication/output-communication.component.ts
+++ src/app/concepts-main/angular-components/component-communication/output-communication/output-communication.component.ts
@@ -41,4 +41,68 @@ export class OutputCommunicationComponent {
       }
     }
   `;
+
+  codeOutputFunction: string = `
+    import { Component, output} from '@angular/core';
+
+    @Component({
+      selector: 'app-child',
+      template: '<button (click)="sendMessage()">Send Message to Parent</button>'
+    })
+    export class ChildComponent {
+      messageToParent = output<string>();
+
+      sendMessage() {
+        this.messageToParent.emit('Hello from your child');
+      }
+    }
+  `;
+
+  codeOuputAlias: string = `
+    @Component({...})
+    export class MyCoolComponent {
+      @Output('somePropertyAlias') propertyName = new EventEmitter<string>();
+    }
+
+    // Template
+    <my-cool-component (somePropertyAlias)="acceptChildEvent()"></my-cool-component>
+  `;
+
+  codeOutputDecorator: string = `
+  @Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  outputs: ['outputProperty: outputPropertyAlias'],
+  })
+  export class MyComponent extends MyBaseComponent {}
+
+  // MyComponent inherits the outputProperty property from MyBaseComponent
+  `;
+
+  codeOuputNaming: string = `
+  @Component({
+    selector: 'app-my-component',
+    template: '<div (click)="handleClick()"></div>',
+    outputs: ['click']  // This should be avoided
+  })
+    export class MyComponent {
+      @Output() click = new EventEmitter<void>();  // This should be avoided
+  }
+
+  //DO NOT DO WHAT IS SHOW ABOVE
+  `;
+
+  codeOutputObservable: string = `
+    import { outputFromObservable } from '@angular/core';
+
+    @Component({
+      selector: 'app-timer-component',
+      template: '<p>{{ seconds }}</p>',
+      standalone: true,
+    })
+    export class TimerComponent {
+      seconds$ = interval(1000); // Observable emitting values every second
+      seconds = outputFromObservable(this.seconds$);
+    }
+  `;
 }
