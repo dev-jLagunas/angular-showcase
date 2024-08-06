23ae2381d5bd473b1cb80a5fcc946a2df2598f5e
Juan Lagunas <dev.jlagunas@gmail.com>
2024-08-07 08:43:35 +0900

    feat(BasicContentProjection): complete add html content and styles

--- src/app/concepts-main/angular-components/content-projection/basic-content-projection/basic-content-projection.component.ts
+++ src/app/concepts-main/angular-components/content-projection/basic-content-projection/basic-content-projection.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CodeBlockComponent } from '../../../../shared/code-block/code-block.component';
 
 @Component({
   selector: 'app-basic-content-projection',
   standalone: true,
-  imports: [],
+  imports: [CodeBlockComponent],
   templateUrl: './basic-content-projection.component.html',
-  styleUrl: './basic-content-projection.component.scss'
+  styleUrl: './basic-content-projection.component.scss',
 })
 export class BasicContentProjectionComponent {
+  codeBasicProjectionChild: string = `
+    import { Component } from '@angular/core';
 
+    @Component({
+      selector: 'app-custom-greeting',
+      template: \`
+        <div class="greeting">
+          <ng-content></ng-content>
+        </div>
+      \`,
+    })
+    export class CustomGreetingComponent {}
+
+  `;
+
+  codeBasicProjectionParent: string = `
+    import { Component } from '@angular/core';
+    import { CustomGreetingComponent } from './custom-greeting.component';
+
+    @Component({
+      selector: 'app-parent',
+      template: \`
+        <app-custom-greeting>
+          <h2>Hello, User!</h2>
+          <p>Welcome to our website. We hope you enjoy your visit.</p>
+        </app-custom-greeting>
+      \`,
+      standalone: true,
+      imports: [CustomGreetingComponent]
+    })
+    export class ParentComponent {}
+  `;
 }
--- src/app/concepts-main/angular-components/content-projection/content-projection.component.ts
+++ src/app/concepts-main/angular-components/content-projection/content-projection.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CodeBlockComponent } from '../../../shared/code-block/code-block.component';
+import { BasicContentProjectionComponent } from './basic-content-projection/basic-content-projection.component';
+import { AdvancedContentProjectionComponent } from './advanced-content-projection/advanced-content-projection.component';
+import { MultipleSlotsProjectionComponent } from './multiple-slots-projection/multiple-slots-projection.component';
 
 @Component({
   selector: 'app-content-projection',
   standalone: true,
-  imports: [CodeBlockComponent],
+  imports: [
+    CodeBlockComponent,
+    BasicContentProjectionComponent,
+    AdvancedContentProjectionComponent,
+    MultipleSlotsProjectionComponent,
+  ],
   templateUrl: './content-projection.component.html',
   styleUrl: './content-projection.component.scss',
 })
